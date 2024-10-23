9cf72b3ade0a14ce9843ed546dc050fee6478f5a
cold <kholodihor@gmail.com>
2024-10-23 13:58:34 +0300

    fix: try to fix pool

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,8 @@
 /* eslint-disable ts/ban-ts-comment */
-import { handle } from "hono/vercel";
+import { handle } from "@hono/node-server/vercel";
 
 /* eslint-disable antfu/no-import-dist */
 // @ts-expect-error
 import app from "../dist/src/app.js";
 
-export const runtime = "edge";
-
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const PATCH = handle(app);
-export const DELETE = handle(app);
-export const OPTIONS = handle(app);
-export const HEAD = handle(app);
+export default handle(app);
