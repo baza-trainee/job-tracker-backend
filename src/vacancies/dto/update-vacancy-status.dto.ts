3d5fda54d76285bac5b408181e5ea717c56d03db
cold <kholodihor@gmail.com>
2024-12-24 11:08:33 +0200

    fox: fix update status

--- src/vacancies/dto/update-vacancy-status.dto.ts
+++ src/vacancies/dto/update-vacancy-status.dto.ts
@@ -29,13 +29,4 @@ export class UpdateVacancyStatusDto {
   @IsUUID()
   @IsOptional()
   resumeId?: string;
-
-  @ApiProperty({
-    description: 'Status ID (required only when updating existing status)',
-    required: false,
-    example: '123e4567-e89b-12d3-a456-426614174000'
-  })
-  @IsUUID()
-  @IsOptional()
-  statusId?: string;
 }
